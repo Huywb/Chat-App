cc9a67b30810de1c141fdfa5eb9bfa093ee59a4d
huywb <pthhuy1306@gmail.com>
2025-03-05 16:26:47 +0700

    Front-end Set up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Box } from '@mui/material'
+import { Route, Routes } from 'react-router-dom'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import SettingsPage from './pages/SettingsPage'
+import ProfilePage from './pages/ProfilePage'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box display='flex' justifyContent='center'>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/signup' element={<Signup></Signup>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
+          <Route path='/settings' element={<SettingsPage></SettingsPage>}></Route>
+          <Route path='/profile' element={<ProfilePage></ProfilePage>}></Route>
+        </Routes>
+    </Box>
   )
 }
 
