5b47a330619d5984fca22a14a3e8645ecd2ae0fe
huywb <pthhuy1306@gmail.com>
2025-03-05 14:35:44 +0700

    GetUser logic

--- backend/src/controllers/messageController.js
+++ backend/src/controllers/messageController.js
@@ -1,5 +1,19 @@
+import User from "../models/user"
 
 
 export const sendMessage = (req,res)=>{
 
+}
+
+export const getUser = async(req,res)=>{
+    try {
+        const userId = req.user._id
+        const GetallUser = await User.find()
+        const FilterUser = GetallUser.map((user)=> user._id !== userId)
+
+        res.status(200).json({message:"Get user success",data: FilterUser})
+    } catch (error) {
+        console.log("Error in getUser controller",error.message)
+        res.status(500).json({message:"Internal Server Error"})
+    }
 }
--- backend/src/routes/message.js
+++ backend/src/routes/message.js
@@ -6,6 +6,7 @@ const router = express.Router()
 
 
 router.post('/send',protectedRoute,sendMessage)
+router.get('/get-user',protectedRoute,getUser)
 
 
 export default router
